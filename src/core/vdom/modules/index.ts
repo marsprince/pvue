48e22a2843f8b0dc3e76f741c0cbc7d691514ca2
marsprince <marsprince@users.noreply.github.com>
2019-09-25 15:10:25 +0800

    feat(directives): support ref

--- src/core/vdom/modules/index.ts
+++ src/core/vdom/modules/index.ts
@@ -1,9 +1,11 @@
-import { Hooks } from "./hooks.enum";
 import { updateDirectives } from "./directives";
+import { IModuleHooks } from "../../../@types/hooks";
+import ref from "./ref";
 
-export const hook = {
-  [Hooks.Create]: [updateDirectives],
-  [Hooks.Update]: [updateDirectives]
+export const hook: IModuleHooks = {
+  create: [updateDirectives, ref.create],
+  update: [updateDirectives, ref.update],
+  destroy: [ref.destroy]
 };
 
 export default {
