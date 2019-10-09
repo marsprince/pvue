02194cda958c98a5ad86728fbe1c9c17ea76f7ca
marsprince <marsprince@users.noreply.github.com>
2019-10-09 19:28:02 +0800

    feat(class):support :class

--- src/platforms/backend/modules/index.ts
+++ src/platforms/backend/modules/index.ts
@@ -1,13 +1,14 @@
 import { updateDOMListeners } from "./events";
 import { updateDomProps } from "./domProps";
+import { updateClass } from "./class";
 import { Hooks } from "../../../core/vdom/modules/hooks.enum";
 import { hook as baseHooks } from "../../../core/vdom/modules";
 
 export const hook = {
-  [Hooks.Create]: [updateDOMListeners, updateDomProps].concat(
+  [Hooks.Create]: [updateDOMListeners, updateDomProps, updateClass].concat(
     baseHooks[Hooks.Create]
   ),
-  [Hooks.Update]: [updateDOMListeners, updateDomProps].concat(
+  [Hooks.Update]: [updateDOMListeners, updateDomProps, updateClass].concat(
     baseHooks[Hooks.Update]
   )
 };
